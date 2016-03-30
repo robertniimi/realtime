b70fe031b454f51cd2ba8ec027781a48105b0884
Robert <robert@engagio.com>
2016-03-30 11:23:50 -0700

    adds server and gulpfile

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ var
   app = express(),
   path = require('path');
 
+// if (process.env.NODE_ENV === 'development') {
+//   const webpack = require('webpack');
+//   const webpackConfig = require('./app/webpack.config');
+//   const compiler = webpack(webpackConfig);
+
+//   app.use(require('webpack-dev-middleware')(compiler, {
+//     noInfo: true,
+//     publicPath: webpackConfig.output.publicPath,
+//   }));
+
+//   app.use(require('webpack-hot-middleware')(compiler));
+// };
+
 app.use(express.static(__dirname + '/dist/'));
 
 app.get('/', (req, res) => {
